b6b970f73d8e29db4cff20be4d6cc4ed9c0cb091
Sambashivarao-Boyina <boyinasambashivarao@gmail.com>
2024-01-23 10:20:46 +0530

    created user profile route

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
 const express=require("express");
 const router=express.Router();
 const User=require("../models/user");
-
+const Post=require("../models/post");
 
 router.get("/:id",async (req,res)=>{
-    let {id} = req.params;
-    const user=await User.findById(id);
-    res.render("user/userProfile.ejs",{user});
-});
+            let {id} = req.params;
+            const user=await User.findById(id);
+            const posts=await Post.find({owner:user._id})
+            res.render("user/userProfile.ejs",{user,posts});
+        });
 
 module.exports=router;
