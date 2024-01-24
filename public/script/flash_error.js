a311ffe0d6e75b34d648564afc84bfda41d2fae7
Sambashivarao-Boyina <boyinasambashivarao@gmail.com>
2024-01-24 09:59:02 +0530

    Flash error message problem is fixed

---
+++ public/script/flash_error.js
@@ -0,0 +1,7 @@
+const error=document.querySelector(".error");
+
+const errorBtn=document.querySelector(".error-btn");
+
+errorBtn.addEventListener("click",(event)=>{
+    error.style.display="none";
+});
