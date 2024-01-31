79918b12449d15e986148ecb39a6f9fc6bcb46b7
Sambashivarao-Boyina <boyinasambashivarao@gmail.com>
2024-01-31 12:08:24 +0530

    media queries added to the home page and users page

--- public/script/navbar.js
+++ public/script/navbar.js
@@ -1,12 +1,1 @@
-
-const navBar=document.querySelector(".nav-options");
-const navItems=document.querySelector(".nav-items");
-
-
-navBar.addEventListener("click",(event)=>{
-    if(navItems.style.height=="0px"){
-        navItems.style.height="auto";
-    }else{
-        navItems.style.height="0px";
-    }
-});
+    
